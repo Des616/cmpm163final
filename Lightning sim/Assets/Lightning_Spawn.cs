02d54b63dbbe316a4b56a87d748ce2ff2606401f
Nathaniel Pentico <nathanpentico616@gmail.com>
2020-05-24 13:19:33 -0700

    Using LIne renderer, lightning spawner is in
    Have to find a way to not scuff it as soon as you try to delete old bolts, it breaks if it does right now but just dont let it run for too long and it works

---
+++ Lightning sim/Assets/Lightning_Spawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning_Spawn : MonoBehaviour
+{
+	public Lightning lightning;
+
+	private GameObject source;
+
+	private Vector3 sourcePosition;
+    // Start is called before the first frame update
+   IEnumerator Start(){
+   //	source = GameObject.Find("LightningSource");  // set the source of the lightning to the source object
+  //      sourcePosition= source.transform.position;
+   	while(true){
+   		Instantiate(lightning,sourcePosition,Quaternion.identity);
+  		Instantiate(lightning,sourcePosition,Quaternion.identity);
+ 	//	Instantiate(lightning,sourcePosition,Quaternion.identity);
+ 		yield return null;
+  		
+   	}
+   }
+}
