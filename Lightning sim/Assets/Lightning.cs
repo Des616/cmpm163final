49509db5707d75df9a7b61ad1594a492acf47c4a
Nathaniel Pentico <nathanpentico616@gmail.com>
2020-05-24 18:51:09 -0700

    Added customization for number of bolts, and time between strikes
    Cool features, still lots of work to do

--- Lightning sim/Assets/Lightning.cs
+++ Lightning sim/Assets/Lightning.cs
@@ -16,12 +16,13 @@ public class Lightning : MonoBehaviour
 
    private Vector3 currentPos; //where we are currently
 
-
+   private float maxDif; //holds 1/2 range of values that vertex can change from point to point
 
 
     void Start()
     {
     	lineRenderer = GetComponent<LineRenderer>();
+    	maxDif = 1.5f;
 
     	lineRenderer.positionCount = numSegs;
 
@@ -33,7 +34,7 @@ public class Lightning : MonoBehaviour
         
 
     	for(int i = 1; i < numSegs ; ++i){
-    		float locX = Random.Range(-1.0f,1.0f);
+    		float locX = Random.Range(-1.3f,1.3f);
     		lineRenderer.SetPosition(i,currentPos = new Vector3(currentPos.x - locX,currentPos.y - Random.Range(1.0f,2.0f),currentPos.z - Random.Range(-1.0f,1.0f)));
     	}
     	lineRenderer.SetPosition(0,sourcePosition);
@@ -46,7 +47,7 @@ public class Lightning : MonoBehaviour
 
     {
 	
-	    	color.a -= 10f * Time.deltaTime;
+	    	color.a -= 2f * Time.deltaTime;
       lineRenderer.SetColors(color,color);
     	if(color.a <= 0f && this.gameObject){
     		Destroy(this.gameObject);
--- Lightning sim/Assets/Lightning_Spawn.cs
+++ Lightning sim/Assets/Lightning_Spawn.cs
@@ -8,9 +8,22 @@ public class Lightning_Spawn : MonoBehaviour
 
 	private GameObject source;
 
+	private float seconds;
+
 	private Vector3 sourcePosition;
+
+	public int numBolts;   //number of bolts each strike has
+
+	public int timeBetween;  //seconds between each bolt generation
+
+
+
+	void Start(){
+		        seconds = 0.0f;
+
+	}
     // Start is called before the first frame update
-   IEnumerator Start(){
+  /* IEnumerator Start(){
    //	source = GameObject.Find("LightningSource");  // set the source of the lightning to the source object
   //      sourcePosition= source.transform.position;
    	while(true){
@@ -20,5 +33,17 @@ public class Lightning_Spawn : MonoBehaviour
  		yield return null;
   		
    	}
+   }*/
+
+   void Update(){
+   	seconds += Time.deltaTime;
+    	if(seconds>=timeBetween){
+    		seconds = 0;
+    		for(int i = 0; i < numBolts; i++){
+    		Instantiate(lightning,sourcePosition,Quaternion.identity);
+  			}
+    		print("2s ");
+    	//updateLineRenderer();
+    	}
    }
 }
