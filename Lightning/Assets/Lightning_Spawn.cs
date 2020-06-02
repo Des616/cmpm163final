cec07b5d10a4e7a53057cc1bd55232591c1b6feb
Nathaniel Pentico <nathanpentico616@gmail.com>
2020-06-02 11:17:44 -0700

    Lightning generator now moves aorund the scene each strike

--- Lightning/Assets/Lightning_Spawn.cs
+++ Lightning/Assets/Lightning_Spawn.cs
@@ -15,16 +15,24 @@ public class Lightning_Spawn : MonoBehaviour
 	public int numBolts;   //number of bolts each strike has
 	public float timeBetween;  //seconds between each bolt generation
 
+  private Vector3 movementTranslation;
+
 
 
 	void Start(){
 		        seconds = 0.0f;
+            movementTranslation = transform.position;
+            Debug.Log(movementTranslation);
+
 
 	}
  
    void Update(){
    	seconds += Time.deltaTime;
     	if(seconds>=timeBetween){
+        movementTranslation.x = Random.Range(-15f,15f);
+        movementTranslation.z = Random.Range(-10f,-6f);
+        transform.position=movementTranslation;
     		seconds = 0;
     		for(int i = 0; i < numBolts; i++){
     			Instantiate(lightning,sourcePosition,Quaternion.identity);
